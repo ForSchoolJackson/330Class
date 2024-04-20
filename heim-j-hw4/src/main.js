9163fa6df0d46294cf155aaa96678762bf566efb
jackson <jacksontheim@gmail.com>
2024-04-19 21:52:18 -0400

    homeowrk 4

--- heim-j-hw4/src/main.js
+++ heim-j-hw4/src/main.js
@@ -9,6 +9,7 @@ let geojson;
 
 //favorites array
 let favoriteIds = ["p20", "p79", "p180", "p43"];
+let currentId;
 
 
 // II. Functions
@@ -45,11 +46,19 @@ const getFeatureByID = (id) => {
 
 //show page details
 const showFeatureDetails = (id) => {
-	console.log(`showFeatureDetails - id=${id}`);
+	//console.log(`showFeatureDetails - id=${id}`);
 	const feature = getFeatureByID(id);
 	document.querySelector("#details-1").innerHTML = `Info for ${feature.properties.title}`;
 	document.querySelector("#details-2").innerHTML = `<b>Address: </b> ${feature.properties.address} <br> <b>Phone: </b> ${feature.properties.phone} <br> <b>Websire</b> ${feature.properties.url}`;
 	document.querySelector("#details-3").innerHTML = feature.properties.description
+
+	//show buttons
+	document.querySelector("#buttons").style.display = "block";
+
+	//get the current id
+	currentId = id;
+
+	changeButtonStates();
 };
 
 //refresh the favorites
@@ -79,6 +88,66 @@ const createFavoriteElement = (id) => {
 	${feature.properties.title}`;
 	return a;
 }
+
+//disable and enable buttons
+const changeButtonStates = () => {
+	if (favoriteIds.includes(currentId)) {
+		favoriteButton.disabled = true;
+		deleteButton.disabled = false; 
+	} else {
+		favoriteButton.disabled = false; 
+		deleteButton.disabled = true; 
+	}
+}
+
+//BUTTON EVENTS
+//favorite button
+const favoriteButton = document.querySelector("#btn-fav");
+favoriteButton.addEventListener("click", () => {
+
+	if (favoriteIds.length > 0) {
+		//search for duplicate
+		for (let i = 0; i < favoriteIds.length; i++) {
+			//leave loop if there is a duplicate
+			if (currentId === favoriteIds[i]) {
+				break;
+			}
+			//only add it once
+			if (i === favoriteIds.length - 1) {
+				//console.log("not yet added")
+				favoriteIds.push(currentId);
+			}
+		}
+	} else {
+		favoriteIds.push(currentId);
+	}
+
+	refreshFavorites();
+	changeButtonStates();
+
+	//console.log("array: " + favoriteIds)
+
+	
+})
+//delete button
+const deleteButton = document.querySelector("#btn-delete");
+deleteButton.addEventListener("click", () => {
+
+	//search for duplicates
+	for (let i = 0; i < favoriteIds.length; i++) {
+		//if one exists
+		if (currentId === favoriteIds[i]) {
+			favoriteIds.splice(i, 1);
+
+		}
+	}
+	refreshFavorites();
+	changeButtonStates();
+
+	//console.log("array: " + favoriteIds)
+	
+
+})
 
 const init = () => {
 	map.initMap(lnglatNYS);
